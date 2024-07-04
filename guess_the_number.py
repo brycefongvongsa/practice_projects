3076965e4f4cbfae0a386244f666898cee5431d0
Bryce Fongvongsa <BryceF24@gmail.com>
2024-07-03 23:45:01 -0500

    guess the number

---
+++ guess_the_number.py
@@ -0,0 +1,29 @@
+import random, sys
+
+## setup
+secretNum = random.randint(1, 100)
+numOfTries = 6
+userInput = 0
+
+# start of game
+print("\nGuess the number from 1 to 100!")
+print(f"You have {numOfTries} attempts.")
+while numOfTries != 0:
+    userInput = int(input())
+    if userInput > 100 or userInput < 0:
+        print("it not within range pls try again")
+    else:  
+        if userInput != secretNum:
+            numOfTries = numOfTries - 1
+            if userInput > secretNum:
+                print(f"Guess lower. You have {numOfTries} tries left.")
+            elif userInput < secretNum:
+                print(f"Guess higher. You have {numOfTries} tries left.")
+        else:
+            sys.exit("Good job! You guessed right!")
+            
+## game over, end game
+if numOfTries == 0:
+    sys.exit(f"Game over. The number was {secretNum}.")
+
+    
